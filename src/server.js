19494a2549dd8b5236b674ff2adb511a8d58bf88
beejay321 <jibodu.busola@gmail.com>
2021-10-14 16:36:46 +0200

    Implementing socket.io

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import listEndpoints from "express-list-endpoints";
 import { createServer } from "http";
+import { Server } from "socket.io";
 import usersRouter from "./models/users/index.js";
 import projectsRouter from "./models/projects/index.js";
 import chatRouter from "./models/Room/index.js";
